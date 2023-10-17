71ea365c03f50da4e74a1246e8c364ae1422212b
Younes Outerbah <younesouterbah1@gmail.com>
2023-10-17 17:41:08 +0100

    deleted everything fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import { About } from "./components/about/About";
-import { Footer } from "./components/footer/Footer";
-import { Header } from "./components/header/Header";
-import { Landing } from "./components/landing/Landing";
-import { Projects } from "./components/projects/Projects";
-import { useDarkMode } from "./context/DarkModeProvider";
+
 
 function App() {
-  const { darkMode } = useDarkMode();
   return (
     <>
-      <div className={`${darkMode && "bg-[#f3f3f3]"}`}>
-        <Header />
-        <Landing />
-        <About />
-        <Projects />
-        <Footer />
-      </div>
     </>
   );
 }
